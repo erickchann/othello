6022b270af914e873c4402142a82533038a66d6d
Erick Chandra <erickchandra718@gmail.com>
2021-10-11 20:56:47 +0700

    update

--- src/Game.js
+++ src/Game.js
@@ -1,213 +1,342 @@
 class Game {
-    constructor() {
-        this.score = {
-            p: 2,
-            c: 2
-        };
+    constructor(option) {
+        this.end = false;
 
-        this.white = 1;
-        this.black = 2;
+        this.black = 1;
+        this.white = 2;
         this.legal = 3;
 
-        this.player = this.white;
-        this.comp = this.black;
+        this.player = option;
+        this.computer = (this.player == this.black) ? this.white : this.black;
 
-        this.turn = this.player;
+        this.turn = (this.computer == this.black) ? this.computer : this.player;
 
         this.board = [
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, this.player, this.comp, 0, 0, 0],
-            [0, 0, 0, this.comp, this.player, 0, 0, 0],
+            [0, 0, 0, this.player, this.computer, 0, 0, 0],
+            [0, 0, 0, this.computer, this.player, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0]
         ];
 
-        this.directions = [
-            [0, -1], // top
-            [0, 1], // bottom
-            [-1, 0], // left
-            [1, 0], // right
-            [1, 1], // bottom right
-            [-1, -1], // top left
-            [-1, 1], // bottom left
-            [1, -1], // top right
+        this.dir = [
+            [0, -1],
+            [0, 1],
+            [-1, 0],
+            [1, 0],
+            [1, 1],
+            [-1, -1],
+            [-1, 1],
+            [1, -1]
         ];
 
+        this.init();
+    }
+
+    /**
+     * Init Function to trigger update and listener function
+     */
+    init() {
         this.update();
-        this.legalMove();
         this.listener();
+
+        if (this.turn == this.computer) this.computerMove();
     }
 
-    around(x, y) {
-        this.directions.forEach(val => {
-            let [dirX, dirY] = val;
+    /**
+     * Function to update all game state
+     */
+    update() {
+        this.drawBoard();
+        this.drawPiece();
+        this.removeLegal()
 
-            let data;
-            if (data = this.check(x, y, dirX, dirY)) {
-                for (let i = 0; i < data.length; i++) {
-                    this.board[data[i].y][data[i].x] = this.turn;
-                }
-            }
-        });
-    }
-
-    check(x, y, dirX, dirY) {
-        let coord = [];
-
-        x += dirX;
-        y += dirY;
-
-        while (this.inBoard(x, y) && (this.board[y][x] != 3 && this.board[y][x] != 0)) {
-            coord.push({
-                x: x,
-                y: y
-            });
-
-            if (this.board[y][x] == this.turn) return coord;
-
-            x += dirX;
-            y += dirY;
-        }
-        
-        return false;
-    }
-
-    legalMove() {
-        for (let i = 0; i < len; i++) {
-            for (let j = 0; j < len; j++) {
-                if (this.board[i][j] == this.turn) {
-                    this.directions.forEach(val => {
-                        let [dirX, dirY] = val;
-
-                        this.getLegalMove(j, i, dirX, dirY);
-                    });
-                }
-            }
-        }
-    }
-
-    getLegalMove(x, y, dirX, dirY) {
-        let opponent = (this.turn == this.comp) ? this.player : this.comp;
-
-        x += dirX;
-        y += dirY;
-
-        while (this.inBoard(x, y) && this.board[y][x] == opponent) {
-            x += dirX;
-            y += dirY;
+        if (!this.getLegal()) {
+            this.end = true;
             
-            if (this.inBoard(x, y) && this.board[y][x] == 0) {
-                this.board[y][x] = this.legal;
-                this.update();
-                this.drawLegal();
-                
-                break;
-            }
-
-            if (this.turn == this.player) this.drawLegal();
+            this.getWinner();
         }
+
+        this.countScore();
     }
 
-    removeLegal() {
+    /**
+     * Function to get the winner if game end
+     */
+    getWinner() {
+        let computer = 0;
+        let player = 0;
+        
         this.board.forEach((row, y) => {
             row.forEach((col, x) => {
-                if (col == this.legal) this.board[y][x] = 0;
-                this.draw();
+                if (col == this.player) {
+                    player++;
+                }
+
+                else if (col == this.computer) {
+                    computer++;
+                }
             });
         });
-    }
 
-    inBoard(x, y) {
-        return x >= 0 && y >= 0 && x < len && y < len;
-    }
-
-    isGameOver() {
-        this.legalMove();
-        let count = this.allowed().length;
-
-        return (count == 0) ? true : false;
-    }
-
-    getWinner() {
-        let winner = (this.score.p == this.score.c) ? 'Tie!' : (this.score.p > this.score.c) ? 'You Win!' : 'You Lose!';
+        let winner = (computer > player) ? 'Game End, Computer Win!' : 'Game End, Player Win!';
 
         alert(winner);
     }
 
-    updateScore() {
-        let player = [];
-        let comp = [];
+    /**
+     * Function to count player score in every game state
+     */
+    countScore() {
+        let computer = 0;
+        let player = 0;
+        
+        this.board.forEach((row, y) => {
+            row.forEach((col, x) => {
+                if (col == this.player) {
+                    player++;
+                }
 
-        this.board.forEach(row => {
-            row.forEach(col => {
-                if (col == this.player) player.push(col);
-                else if (col == this.comp) comp.push(col);
+                else if (col == this.computer) {
+                    computer++;
+                }
             });
         });
 
-        this.score.p = player.length;
-        this.score.c = comp.length;
-
-        playerScore.innerHTML = this.score.p;
-        compScore.innerHTML = this.score.c;
+        playerEl.innerHTML = player;
+        computerEl.innerHTML = computer;
     }
 
-    update() {
-        ctx.clearRect(0, 0, w, h);
-        this.drawBg();
-        this.draw();
-        this.updateScore();
+    /**
+     * Reset the legal position in every turn
+     */
+    removeLegal() {
+        this.board.forEach((row, y) => {
+            row.forEach((col, x) => {
+                if (col == this.legal) {
+                    this.board[y][x] = 0;
+                }
+            });
+        });
     }
 
+    /**
+     * Function to get legal coord
+     */
+    getLegal() {
+        let valid = false;
+        let opponent = (this.turn == this.computer) ? this.player : this.computer;
+
+        this.board.forEach((row, y) => {
+            row.forEach((col, x) => {
+                if (col == this.turn) {
+                    this.dir.forEach(val => {
+                        let [dirX, dirY] = val;
+
+                        if (this.inBoard(y + dirY, x + dirX) && this.board[y + dirY][x + dirX] == opponent) {
+                            let tempY = y + dirY;
+                            let tempX = x + dirX;
+
+                            while (this.inBoard(tempY, tempX) && this.board[tempY][tempX] == opponent) {
+                                if (this.inBoard(tempY + dirY, tempX + dirX) && this.board[tempY + dirY][tempX + dirX] == 0) {
+                                    this.board[tempY + dirY][tempX + dirX] = this.legal;
+
+                                    valid = true;
+                                }
+
+                                tempY += dirY;
+                                tempX += dirX;
+                            }
+                        }
+                    });
+                }
+            });
+        });
+
+        if (this.turn == this.player) this.drawLegal();
+
+        return valid;
+    }
+
+    /**
+     * Function to draw legal coord to gameboard
+     */
     drawLegal() {
         this.board.forEach((row, y) => {
             row.forEach((col, x) => {
-                if (col != this.legal) return;
+                let coordX = (x * size) + (size / 2);
+                let coordY = (y * size) + (size / 2);
 
-                ctx.strokeStyle = 'black';
-                ctx.beginPath();
-                ctx.arc((size * x + (size / 2)), (size * y + (size / 2)), size * 0.35, 0, Math.PI * 2);
-                ctx.stroke();
+                if (col == this.legal) {
+                    ctx.beginPath();
+                    ctx.arc(coordX, coordY, size * 0.3, 0, Math.PI * 2);
+                    ctx.stroke();
+                }
             });
         });
     }
 
-    draw() {
+    /**
+     * Function to check coord still in gameboard
+     * 
+     * @param {*} y 
+     * @param {*} x 
+     */
+    inBoard(y, x) {
+        return x >= 0 && y >= 0 && x < gridX && y < gridY;
+    }
+
+    /**
+     * Function to handle player move
+     * 
+     * @param {*} y 
+     * @param {*} x 
+     */
+    playerMove(y, x) {
+        if (this.end) return;
+        if (this.turn != this.player) return;
+        if (this.board[y][x] != this.legal) return;
+
+        this.board[y][x] = this.player;
+        this.replace(y, x);
+
+        this.turn = this.computer;
+        this.update();
+        this.computerMove();
+    }
+
+    /**
+     *  Function to handle computer move
+     */
+    computerMove() {
+        if (this.end) return;
+
+        let coord = this.getLegalCoord();
+        let random = coord[~~(Math.random() * coord.length)];
+        
+        setTimeout(() => {
+            this.board[random.y][random.x] = this.computer;
+            this.replace(random.y, random.x);
+
+            this.turn = this.player;
+            this.update();
+        }, 500);
+    }
+
+    /**
+     * Function to get all legal
+     * 
+     * @returns array
+     */
+    getLegalCoord() {
+        let coord = [];
+
         this.board.forEach((row, y) => {
             row.forEach((col, x) => {
-                if (col == 0 || col == 3) return;
+                if (col == this.legal) {
+                    coord.push({x: x, y: y});
+                }
+            });
+        });
 
-                if (col == this.player) {
-                    ctx.fillStyle = 'white';
-                    ctx.strokeStyle = 'white';
-                } else if (col == this.comp) {
-                    ctx.fillStyle = 'black';
-                    ctx.strokeStyle = 'black';
+        return coord;
+    }
+
+    /**
+     * Function to repleace disc
+     * 
+     * @param {*} y 
+     * @param {*} x 
+     */
+    replace(y, x) {
+        let opponent = (this.turn == this.computer) ? this.player : this.computer;
+
+        this.dir.forEach(val => {
+            let [dirX, dirY] = val;
+
+            let tempY = y + dirY;
+            let tempX = x + dirX;
+
+            while (this.inBoard(tempY, tempX) && this.board[tempY][tempX] == opponent) {
+                if (this.last(tempY, tempX, dirX, dirY)) {
+                    this.board[tempY][tempX] = this.turn;
                 }
 
-                ctx.beginPath();
-                ctx.arc((size * x + (size / 2)), (size * y + (size / 2)), size * 0.35, 0, Math.PI * 2);
-                ctx.stroke();
-                ctx.fill();
+                tempY += dirY;
+                tempX += dirX;
+            }
+        });
+    }
+
+    /**
+     * Function to help replace function to find last disc is same or not
+     * 
+     * @param {*} y 
+     * @param {*} x 
+     * @param {*} dirX 
+     * @param {*} dirY 
+     * @returns bool
+     */
+    last(y, x, dirX, dirY) {
+        let last = false;
+
+        while (this.inBoard(y, x)) {
+            if (this.board[y][x] == this.turn) last = true;
+
+            y += dirY;
+            x += dirX;
+        }
+
+        return last;
+    }
+
+    /**
+     * Function to draw player or computer disc
+     */
+    drawPiece() {
+        this.board.forEach((row, y) => {
+            row.forEach((col, x) => {
+                let coordX = (x * size) + (size / 2);
+                let coordY = (y * size) + (size / 2);
+
+                if (col == this.black) {
+                    ctx.fillStyle = 'black';
+                    ctx.beginPath();
+                    ctx.arc(coordX, coordY, size * 0.3, 0, Math.PI * 2);
+                    ctx.fill();
+                }
+                
+                else if (col == this.white) {
+                    ctx.fillStyle = 'white';
+                    ctx.beginPath();
+                    ctx.arc(coordX, coordY, size * 0.3, 0, Math.PI * 2);
+                    ctx.fill();
+                }
             });
         });
     }
 
-    drawBg() {
-        ctx.strokeStyle = 'black';
-        // vertical
-        for (let i = 0; i <= 8; i++) {
+    /**
+     * Function to draw Board State
+     */
+    drawBoard() {
+        // draw bg
+        ctx.fillStyle = 'rgb(14, 72, 82)';
+        ctx.fillRect(0, 0, w, h);
+
+        // vertical line
+        for (let i = 0; i < gridY; i++) {
             ctx.beginPath();
             ctx.moveTo(i * size, 0);
             ctx.lineTo(i * size, h);
             ctx.stroke();
         }
 
-        // horizontal
-        for (let i = 0; i <= 8; i++) {
+        // horizontal line
+        for (let i = 0; i < gridY; i++) {
             ctx.beginPath();
             ctx.moveTo(0, i * size);
             ctx.lineTo(w, i * size);
@@ -215,58 +344,15 @@ class Game {
         }
     }
 
+    /**
+     * Function to listen to player click
+     */
     listener() {
-        canvas.addEventListener('click', (e) => {
+        canvas.addEventListener('click', e => {
             let x = ~~(e.offsetX / size);
             let y = ~~(e.offsetY / size);
 
-            if (this.board[y][x] == this.legal && this.turn == this.player) {
-                this.board[y][x] = this.player;
-                this.around(x, y);
-                this.turn = this.comp;
-                this.removeLegal();
-                this.update();
-                if (this.isGameOver()) {
-                    this.getWinner();
-                    return;
-                };
-                this.bot();
-                this.update();
-            }
+            this.playerMove(y, x);
         });
-    }
-
-    bot() {
-        let allowed = this.allowed()[~~(Math.random() * this.allowed().length)];
-        
-        setTimeout(() => {
-            this.board[allowed.y][allowed.x] = this.comp;
-            this.around(allowed.x, allowed.y);
-            this.turn = this.player;
-            this.removeLegal();
-            if (this.isGameOver()) {
-                this.getWinner();
-                this.update();
-
-                return;
-            };
-            this.update();
-            this.drawLegal();
-        }, 500);
-    }
-
-    allowed() {
-        const coord = [];
-
-        this.board.forEach((row, y) => {
-            row.forEach((col, x) => {
-                if (col == this.legal) coord.push({
-                    x: x,
-                    y: y
-                });
-            });
-        });
-
-        return coord;
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,70 @@
+const modal = document.querySelector('.modal');
+const modalInner = document.querySelector('.modal-inner');
+const counter = document.querySelector('.timer');
+
+const sound = document.querySelector('#sound');
+const audio = document.querySelector('#audio');
+
+const playerEl = document.querySelector('.player-score');
+const computerEl = document.querySelector('.computer-score');
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
-const playerScore = document.querySelector('#player');
-const compScore = document.querySelector('#comp');
 
 const w = canvas.width;
 const h = canvas.height;
 
-const len = 8;
-const size = w / len;
+const gridX = 8;
+const gridY = 8;
+
+const size = w / 8;
 
 let game;
-window.onload = init();
 
-function init() {
-    game = new Game();
+/**
+ * Function To Init The Game
+ */
+function init(params) {
+    counter.style.display = 'block';
+    modalInner.style.display = 'none';
+
+    let count = 2;
+
+    let interval = setInterval(() => {
+        counter.innerHTML = count;
+        count--;
+        
+        if (count < 0) {
+            modal.style.display = 'none';
+
+            clearInterval(interval);
+            startGame(params);
+        }
+    }, 1000);
+}
+
+/**
+ * Function to start the game
+ * 
+ * @param {*} choose 
+ */
+function startGame(choose) {
+    game = new Game(choose);
+}
+
+/**
+ * Sound Controller
+ */
+sound.addEventListener('change', e => {
+    if (e.target.checked) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+
+function removeDisabled() {
+    document.querySelector('.white').disabled = false;
+    document.querySelector('.black').disabled = false;
 }
